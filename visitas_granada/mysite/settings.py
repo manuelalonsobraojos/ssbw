d0c7f54d66809e602e6a777fab98166647009749
manuel <manuelalonso136@gmail.com>
2020-06-16 22:45:29 +0200

    Configuración de cors

--- visitas_granada/mysite/settings.py
+++ visitas_granada/mysite/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,6 +78,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'visitas_granada', 'media')
 WSGI_APPLICATION = 'mysite.wsgi.application'
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__) ,'../visitas_granada/templates').replace('\\','/'),
